9fd65d8d0bc316d26855da01477f6d682d1b2924
oriol <oriol@invinet.org>
2021-07-09 10:22:53 +0200

    new preprocessed correcting cl-24

--- RuleDev/update-xslt-and-validate.cmd
+++ RuleDev/update-xslt-and-validate.cmd
@@ -4,25 +4,14 @@ export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-13.jdk/Contents/Home
 
 
 rem preprocess first - catches errors quickers
-call mvn -f pom-preprocess.xml generate-resources
-if errorlevel 1 goto error
+mvn -f pom-preprocess.xml generate-resources
 
 rem convert to XSLT - takes forever
-call mvn -f pom-xslt.xml process-resources
-if errorlevel 1 goto error
+mvn -f pom-xslt.xml process-resources
 
 rem Add license headers to all relevant files
-call mvn -f pom-license.xml license:format
-if errorlevel 1 goto error
+mvn -f pom-license.xml license:format
 
 rem validate afterwards
-call mvn -f pom-validate.xml validate
-if errorlevel 1 goto error
+mvn -f pom-validate.xml validate
 
-echo Sucessfully done
-goto end
-
-:error
-pause
-
-:end
